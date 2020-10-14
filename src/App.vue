330a5d3ba9e3f90cb6324bf1df371ce5f8d64ca7
guozhitao <2943439858@qq.com>
2020-10-14 11:58:42 +0800

    feat: 用 reactive 和 toRefs() 优化代码

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,35 @@
       {{index}} : {{item}}
     </div>
   </div>
-  <div class="">被选中的人是： {{selected}}</div>
+  <div class="">{{a}}被选中的人是： {{selected}}</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'; 
+import { reactive, toRefs } from 'vue';  // 使用reactive 优化代码； 使用 toRefs() 优化渲染的时候的 data
 
-export default defineComponent({
+export default ({
   name: 'App',
   components: {
   },
   setup() { // setup 中定义 变量和逻辑
-    const list = ref(['小明', '小张', '小强']); // 要在template 中的使用 必须用ref包装一下
-    const selected = ref('');
-    const select = (index: number) => {
-      selected.value = list.value[index]
-    };
+    interface DataProps { // 定义接口作为类型注解
+      list: string[];
+      selected: string;
+      select: (index: number) => void;
+    }
+    const data: DataProps = reactive({ // data增加类型注解
+      list: ['小明', '小张', '小强'], // 要在template 中的使用 必须用ref包装一下
+      selected: '',
+      select: (index: number) => {
+        data.selected = data.list[index]
+      }
+    })
+
+    const refData = toRefs(data);
+    
 
    return { // 将需要在 template 中使用的 变量 和方法暴露出去
-     list,
-     selected,
-     select
+     ...refData
    };
   },
 });
