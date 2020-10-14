4d46ccaa2321142a7239df182c08b5bee3a5ff23
guozhitao <2943439858@qq.com>
2020-10-14 11:40:16 +0800

    feat: setup() 和 ref()

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="test">
+    <div @click="select(index)" v-for="(item, index) in list" :key="index">
+      {{index}} : {{item}}
+    </div>
+  </div>
+  <div class="">被选中的人是： {{selected}}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'; 
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  },
+  setup() { // setup 中定义 变量和逻辑
+    const list = ref(['小明', '小张', '小强']); // 要在template 中的使用 必须用ref包装一下
+    const selected = ref('');
+    const select = (index: number) => {
+      selected.value = list.value[index]
+    };
+
+   return { // 将需要在 template 中使用的 变量 和方法暴露出去
+     list,
+     selected,
+     select
+   };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { createApp } from 'vue' // 引入vue文件， 并且导出·createApp·
+import App from './App.vue'
+
+createApp(App).mount('#app')
